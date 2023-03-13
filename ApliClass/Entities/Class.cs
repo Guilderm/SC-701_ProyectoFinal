7af27eef1d81f6030fe4c59351ecbb3d4c7d6d24
Guilder W. Milliner <guilderw@gmail.com>
2023-03-13 12:08:45 -0600

    Created all important backend controllers, with there respective postman

--- ApliClass/Entities/Class.cs
+++ ApliClass/Entities/Class.cs
@@ -1,24 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
+﻿namespace Entities;
 
-namespace Entities
+public class Class
 {
-    public partial class Class
+    public Class()
     {
-        public Class()
-        {
-            Assessments = new HashSet<Assessment>();
-            Lessons = new HashSet<Lesson>();
-            Students = new HashSet<Student>();
-        }
-
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public int TeacherId { get; set; }
-
-        public virtual User Teacher { get; set; }
-        public virtual ICollection<Assessment> Assessments { get; set; }
-        public virtual ICollection<Lesson> Lessons { get; set; }
-        public virtual ICollection<Student> Students { get; set; }
+        Assessments = new HashSet<Assessment>();
+        Lessons = new HashSet<Lesson>();
+        Students = new HashSet<Student>();
     }
+
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int TeacherId { get; set; }
+
+    public virtual User Teacher { get; set; }
+    public virtual ICollection<Assessment> Assessments { get; set; }
+    public virtual ICollection<Lesson> Lessons { get; set; }
+    public virtual ICollection<Student> Students { get; set; }
 }
